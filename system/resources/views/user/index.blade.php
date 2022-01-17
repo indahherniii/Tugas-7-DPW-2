a9f7a2a6c9689581660dda995e8872cc3e63353a
Muhammad Khadafi <90744909+indahherniii@users.noreply.github.com>
2022-01-17 02:41:06 -0800

    m

--- system/resources/views/user/index.blade.php
+++ system/resources/views/user/index.blade.php
@@ -8,7 +8,8 @@
                 <div class="card">
                     <div class="card-header">
                         Data User
-                        <a href="{{url('admin/user/create')}}" class="btn btn-dark float-right"><i class="fa fa-plus"></i> Tambah Data</a>
+                        <a href="{{ url('admin/user/create') }}" class="btn btn-dark float-right"><i
+                                class="fa fa-plus"></i> Tambah Data</a>
                     </div>
                     <div class="card-body">
                         <table class="table">
@@ -21,28 +22,30 @@
                                 <th>Email</th>
                             </thead>
                             <tbody>
-                                @foreach($list_user as $user)
-                                <tr>
-                                    <td>{{$loop->iteration}}</td>
-                                    <td>
-                                        <div class="btn-group">
-                                            <a href="{{url('admin/user', $user->id)}}" class="btn btn-dark"><i class="fa fa-info"></i></a>
-                                            <a href="{{url('admin/user', $user->id)}}/edit" class="btn btn-warning"><i class="fa fa-edit"></i></a>
-                                            @include('template.utils.delete', ['url' => url('admin/user', $user->id)])
-                                        </div>
-                                    </td>
-                                    <td>{{$user->username}}</td>
-                                    <td>{{$user->nama}}</td>
-                                    <td>{{$user->produk_count}}</td>
-                                    <td>{{$user->email}}</td>
-                                </tr>
+                                @foreach ($list_user as $user)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>
+                                            <div class="btn-group">
+                                                <a href="{{ url('admin/user', $user->id) }}" class="btn btn-dark"><i
+                                                        class="fa fa-info"></i></a>
+                                                <a href="{{ url('admin/user', $user->id) }}/edit" class="btn btn-warning"><i
+                                                        class="fa fa-edit"></i></a>
+                                                @include('template.utils.delete', ['url' => url('admin/user', $user->id)])
+                                            </div>
+                                        </td>
+                                        <td>{{ $user->username }}</td>
+                                        <td>{{ $user->nama }}</td>
+                                        <td>{{ $user->produk_count }}</td>
+                                        <td>{{ $user->email }}</td>
+                                    </tr>
                                 @endforeach
                             </tbody>
                         </table>
                     </div>
-                </div>  
+                </div>
             </div>
-        </div>   
+        </div>
     </div>
 
 
